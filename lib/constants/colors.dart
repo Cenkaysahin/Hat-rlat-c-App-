1bc6b1f84489b7c9b82b12f51f78e9254a81f9c6
Cenkay Şahin <cenkaysahin@hotmail.com>
2024-04-17 15:19:57 +0300

    Hatirlatici App

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF8DECB4);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF141E46);
+
+const Color tdBGColor = Color(0xFFFFF5E0);
